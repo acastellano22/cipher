17c038d5378e8acceb1f9fab6f9e44c5ba8b4af1
Andrew Castellano (Class of 2024) <240360ac@internal.salesianum.org>
2023-02-27 13:56:08 -0500

    additional changes

--- RouteCipher.java
+++ RouteCipher.java
@@ -22,25 +22,37 @@ public class RouteCipher {
                 }
             }
         }
-
-        for (int i = 0; i < letterBlock.length; i++) {
-            for (int j = 0; j < letterBlock[i].length; j++) {
-                System.out.println(letterBlock[i][j]);
-            }
-        }
     }
 
     public static String encryptMessage(String message) {
-        String[] arr = message.split("");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        int counter = 0;
+        String concatString = "";
+        while (counter < message.length()) {
+            String[][] block = new String[numRows][numCols];
+            for (int i = 0; i < block.length; i++) {
+                for (int j = 0; j < block[i].length; j++) {
+                    if (counter < message.length()) {
+                        block[i][j] = message.substring(counter, counter + 1);
+                        counter++;
+                    } else {
+                        block[i][j] = "A";
+                    }
+                }
+            }
+
+            for (int i = 0; i < block.length; i++) {
+                for (int j = 0; j < block[i].length; j++) {
+                    concatString += block[i][j];
+                }
+            }
         }
-        return null;
+        return concatString;
     }
 
     public static void main(String[] args) {
         String[][] letterBlock = new String[2][3];
         RouteCipher cipher = new RouteCipher(letterBlock);
-        encryptMessage("Hello World");
+
+        System.out.println(encryptMessage("hello world"));
     }
 }
